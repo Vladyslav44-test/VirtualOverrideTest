eba9bba880090f5f97be714d5f879c74bcc13cd1
Vladislav44-test <elertvelert@gmail.com>
2026-09-06 11:36:15 +0300

    Додав клас Safe, який буде представляти сейш, який можна відкрити та забрати вміст сейфу

---
+++ Safe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideTest
+{
+    internal class Safe
+    {
+        private string contents = "precious jewels";
+        private string safeCombination = "12345";
+
+        public string Open(string combinatiopn)
+        {
+            if (combinatiopn == safeCombination) return contents;
+            return "";
+        }
+        public void PickLock(Locksmith lockpicker)
+        {
+            lockpicker.Combination = safeCombination;
+        }
+    }
+}
