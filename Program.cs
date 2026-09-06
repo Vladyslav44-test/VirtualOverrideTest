9b54bed9239696909896075752f7a8e5d1583743
Vladislav44-test <elertvelert@gmail.com>
2026-09-06 11:54:16 +0300

    Додав код методу Main, який створює екземпляри SafeOwner, Safe та JewelThief, потім злодій має викрасти вміст сейфу та повідомити про це

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace VirtualOverrideTest
     {
         static void Main(string[] args)
         {
+            SafeOwner owner = new SafeOwner();
+            Safe safe = new Safe();
+            JewelThief jewelThief = new JewelThief();
+            jewelThief.OpenSafe(safe, owner);
+            Console.ReadKey(true);
         }
     }
 }
