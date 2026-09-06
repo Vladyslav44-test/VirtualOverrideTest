10215cf9c9b8a788a76e2bddf9fb3ea80a100fd1
Vladislav44-test <elertvelert@gmail.com>
2026-09-06 11:50:42 +0300

    Додав клас Locksmith та його субкласс JewelThief, який має перевизначати метод ReturnContents, але фактично цього не робить

---
+++ Locksmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideTest
+{
+    internal class Locksmith
+    {
+        public string Combination { private get; set; }
+
+        public void OpenSafe(Safe safe, SafeOwner owner)
+        {
+            safe.PickLock(this);
+            string safeContents = safe.Open(Combination);
+            ReturnContents(safeContents, owner);
+        }
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
+    }
+
+    class JewelThief : Locksmith
+    {
+        private string stolenJewels;
+
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"I'm stealing the jewels! I stole: {stolenJewels}");
+        }
+    }
+}
